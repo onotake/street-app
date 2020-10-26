6f181d7c5b65e514a008d8a964fbc47afd1f9627
onotake <t_si2@yahoo.co.jp>
2020-10-26 15:24:14 +0900

    ビューの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: :index
   def index
+    @posts = Post.includes(:user).order("created_at DESC")
   end
 end
