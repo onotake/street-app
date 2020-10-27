d19dfbb6a7c6a3629649ab89b91e4c43f1c482d5
onotake <t_si2@yahoo.co.jp>
2020-10-27 22:43:49 +0900

    postモデルのバリデーション

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,12 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
   has_many :post_tags
-  has_many :tags, through :post_tags
+  has_many :tags, through: :post_tags
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :caption, length: { maximum: 1000 }
+  end
 end
