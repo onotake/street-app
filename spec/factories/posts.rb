2a1646970bafb60587b726c988f5baa09a682147
onotake <t_si2@yahoo.co.jp>
2020-11-10 14:49:59 +0900

    FactoryBotのリファクタリング

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :post do
+    association :user
     caption {"これは画像です。"}
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
     end
-    association :user
   end
 end
