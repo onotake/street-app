ce8e812dca6ff5f2540c4be5dbb7590e10b747f3
onotake <t_si2@yahoo.co.jp>
2020-11-11 12:08:27 +0900

    コメントのエラー文表示

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,12 @@
 class CommentsController < ApplicationController
   def create
     comment = Comment.create(comment_params)
-    redirect_to "/posts/#{comment.post.id}" 
+    if comment.valid?
+      comment.save
+      redirect_to "/posts/#{comment.post.id}" 
+    else
+      render "/posts/#{comment.post.id}" 
+    end
   end
 
   private
